fdde4eb16196c7f836322e71c93d126c455ebd8d
KARL ENANO <karlenano31@gmail.com>
2022-11-09 06:58:34 +0800

    Fix deleteModal spec

--- src/tests/components/modal/DeleteModal.spec.tsx
+++ src/tests/components/modal/DeleteModal.spec.tsx
@@ -1,16 +1,24 @@
 /* eslint-disable testing-library/no-render-in-setup */
 /* eslint-disable testing-library/prefer-screen-queries */
 import React from "react";
-import { render } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
 import DeleteModal, { Props } from "../../../components/modal/DeleteModal";
 import { BrowserRouter as Router } from "react-router-dom";
-/* <DeleteModal
-    onHide={() => setDeleteMovieState(false)}
-    handleDelete={confirmDeleteMovie}
-    show={deleteMovieState}
-    /> */
+import { Control } from "../../../pages";
+import thunk from "redux-thunk";
+import {
+  actorsMock,
+  moviesMock,
+  pendingReviewsMock,
+  userMock,
+} from "../mockValues";
+import user from "@testing-library/user-event";
 
 const onHide = () => {};
 const handleDelete = () => {};
@@ -18,13 +26,27 @@ const show: boolean = false;
 
 describe("<DeleteModal/>", () => {
   function renderApp(props: Props) {
-    const initialState = {};
-    const mockStore = configureStore();
+    const initialState = {
+      actor: {
+        actors: actorsMock,
+      },
+      movie: {
+        movies: moviesMock,
+      },
+      review: {
+        pendingReviews: pendingReviewsMock,
+      },
+      user: {
+        accounts: userMock,
+      },
+    };
+    const mockStore = configureStore([thunk]);
     let store;
     store = mockStore(initialState);
     return render(
       <Provider store={store}>
         <Router>
+          <Control />
           <DeleteModal {...props} />
         </Router>
       </Provider>
@@ -39,8 +61,33 @@ describe("<DeleteModal/>", () => {
     });
   });
 
-  test.skip("Should show modal", () => {});
+  it("Should show modal", async () => {
+    const deleteBtn = screen.getAllByRole("button", {
+      name: /\| delete/i,
+    });
+    await user.click(deleteBtn[0]);
 
-  test.skip("Should hide when clicking cancel btn", () => {});
-  test.skip("Should confirm when clicking ok btn", () => {});
+    const confirm = screen.getByText(/confirm deletion/i);
+    const ok = screen.getByText(/ok/i);
+    const cancel = screen.getByText(/cancel/i);
+
+    expect(confirm).not.toBeNull();
+    expect(ok).not.toBeNull();
+    expect(cancel).not.toBeNull();
+  });
+
+  test("Should hide when clicking cancel btn", async () => {
+    const deleteBtn = screen.getAllByRole("button", {
+      name: /\| delete/i,
+    });
+    await user.click(deleteBtn[0]);
+
+    const cancel = await screen.findByText(/cancel/i);
+    await user.click(cancel);
+    await waitForElementToBeRemoved(() =>
+      screen.queryByText(/confirm deletion/i)
+    );
+    const confirm = screen.queryByText(/confirm deletion/i);
+    expect(confirm).toBeNull();
+  });
 });
